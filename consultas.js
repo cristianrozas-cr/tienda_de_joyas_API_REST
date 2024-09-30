5aa4bef96c522be3983d4723a3b43088699ef0ff
Cristian Rozas <cristianrozas.cr@gmail.com>
2024-09-30 18:09:55 -0300

    First commit

---
+++ consultas.js
@@ -0,0 +1,49 @@
+const { Pool } = require('pg')
+const format = require('pg-format')
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    password: "12345",
+    database: "joyas",
+    allowExitOnIdle: true
+});
+
+const obtenerJoyas = async ({limit = 15, order_by = "stock_ASC" , page = 1}) => {
+
+    const [campo, direccion] = order_by.split("_")
+    const offset = (page - 1) * limit;
+    let formattedQuery = format("SELECT * FROM inventario ORDER BY %s %s OFFSET %s LIMIT %s", campo, direccion, offset, limit)
+    const { rows: inventario } = await pool.query(formattedQuery)
+    return inventario;
+}
+
+const obtenerJoyasPorFiltros = async ({precio_min, precio_max, categoria, metal}) => {
+
+    let filtros = [];
+
+    console.log("Filtros iniciales: ", filtros)
+
+    if (precio_min) filtros.push(`precio >= ${precio_min}`)
+    if (precio_max) filtros.push(`precio <= ${precio_max}`)
+    if (categoria) filtros.push(`categoria = '${categoria}'`)
+    if (metal) filtros.push(`metal = '${metal}'`)
+
+    console.log("Filtros luego del push si es que existen parámetros: ", filtros)
+
+    let consulta = "SELECT * FROM inventario"
+    if (filtros.length > 0){
+        filtros = filtros.join(" AND ")
+        consulta += ` WHERE ${filtros}`
+    }
+
+    console.log("Array de Filtros luego de la consulta: ", filtros)
+
+    console.log("Consulta: ", consulta)
+
+    const { rows: inventario } = await pool.query(consulta)
+    return inventario;
+
+}
+
+module.exports = { obtenerJoyas, obtenerJoyasPorFiltros }
---
+++ index.js
@@ -0,0 +1,34 @@
+//Importar módulos
+const express = require('express')
+const cors = require('cors')
+
+
+//Importar funciones
+const { obtenerJoyas, obtenerJoyasPorFiltros } = require('./consultas')
+
+//Levantar el servidor
+const PORT = 3000;
+const app = express();
+app.listen(PORT, console.log("Puerto encendido"));
+
+
+//Rutas
+app.get("/joyas", async (req, res) => {
+    try {
+        const queryStrings = req.query;
+        const joyas = await obtenerJoyas(queryStrings)
+        res.json(joyas)
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+app.get("/joyas/filtros", async (req, res) => {
+    try{
+        const queryStrings = req.query;
+        const joyasFiltradas = await obtenerJoyasPorFiltros(queryStrings)
+        res.json(joyasFiltradas)
+    } catch (error) {
+        res.json(error)
+    }
+})
